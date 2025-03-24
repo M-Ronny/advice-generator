570d48e9818ce8825efeec1af9a31bd8e8c4878f
Ronaldo <ronaldomorales.ai@gmail.com>
2025-03-24 16:31:52 -0400

    created html, css, and js file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+fetch("https://api.adviceslip.com/advice")
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
