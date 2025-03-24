f002d1cd398c5754f3fab7e23de94106c40658c7
Ronaldo <ronaldomorales.ai@gmail.com>
2025-03-24 16:56:10 -0400

    added an event listener on button to run the function and display advice

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-fetch("https://api.adviceslip.com/advice")
+document.querySelector('button').addEventListener('click', getAdvice)
+
+function getAdvice() {
+  fetch("https://api.adviceslip.com/advice")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data)
+      document.querySelector('h2').innerText = data.slip.advice
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
+}
